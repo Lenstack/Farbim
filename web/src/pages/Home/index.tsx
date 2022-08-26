4c50f639693512bd0abb0e1a392fe23f7dc7b022
LEONARDO OSPINA <42081538+Lenstack@users.noreply.github.com>
2022-08-26 09:00:15 -0500

    update home page

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,9 +1,12 @@
 import {Wrapper} from "@/components";
+import {Link} from "react-router-dom";
+import {PUBLIC_ROUTES} from "@/constants";
 
 export const Home = () => {
     return (
         <Wrapper>
             Home
+            <Link to={PUBLIC_ROUTES.SIGN_IN}/>
         </Wrapper>
     )
 }
